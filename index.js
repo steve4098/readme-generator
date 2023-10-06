3c5377e87a93b1bb0649e747e34418ece294a3a2
steve4098 <139075699+steve4098@users.noreply.github.com>
2023-10-06 19:26:10 +0100

    const questions populated

--- index.js
+++ index.js
@@ -5,7 +5,53 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    {
+        type: "input",
+        name: "Github",
+        message: "What is your Github username?"
+    },
+    {
+        type: "input",
+        name: "projectName",
+        message: "What is the name of your project?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address?"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "What type of license is your project using?",
+        choices: ["MIT","APACHE v2.0","GNU General Public License v3.0", "Creative Commons Zero v1.0", "Eclipse Public License 2.0", "None"]
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Write a brief description of your project"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Write a brief explanation of how to use your application"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Briefly explain how to install dependencies",
+        default: "npm i"
+    },
+    {
+        type: "input",
+        name: "issues",
+        message: "How should a user report an issue with your application?"
+    },
+    {
+        type: "input",
+        name: "contribute",
+        message: "How can users make contributions to your project?"
+    },
 ];
 
 // function to write README file
