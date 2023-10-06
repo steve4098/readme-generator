02dd646312d97b1da0bf6075bb6f561defc830b6
steve4098 <139075699+steve4098@users.noreply.github.com>
2023-10-06 18:24:32 +0100

    function created to render license badge and call for it in generate Mrkdown function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,14 @@
 // function to generate markdown for README
+
+function renderLicenseBadge (license) {
+  if (license !== "none") {
+    return `![GitHub license](https://img.shields.io/badge/license-${license}-green)`
+  } else {return ""};
+}
+
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
 
 `;
 }
